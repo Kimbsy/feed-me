787ea3035995d1e7d0f958f74b0b625d2c9881ac
Kimbsy <lordkimber@gmail.com>
2016-03-02 18:17:42 +0000

    sending emails properly now

--- src/emailer.py
+++ src/emailer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import os
 import smtplib
 
@@ -7,8 +9,7 @@ class Emailer:
 
     def __init__(self, meals):
         self.meals     = meals
-        self.message   = self.get_message()
-        self.to_addr   = 'dave.kimber@simitive.com'
+        self.to_addr   = 'lordkimber@gmail.com'
         self.from_addr = 'lordkimber@gmail.com'
 
     def get_message(self):
@@ -17,16 +18,21 @@ class Emailer:
             string = meal.show(string)
             string = string + '\n'
 
-        return string
+        message = MIMEText(string, 'plain')
+        message['to'] = self.to_addr
+        message['From'] = self.from_addr
+        message['Subject'] = 'Shopping list'
+
+        return message
 
     def send_list(self):
         username = 'lordkimber@gmail.com'
         password = self.get_password()
 
-        server = smtplib.SMTP('smtp.gmail.com:587')
+        server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login(username, password)
-        server.sendmail(self.from_addr, self.to_addr, self.message)
+        server.sendmail(self.from_addr, self.to_addr, self.get_message().as_string())
         server.quit()
 
     def get_password(self):
